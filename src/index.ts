0ea70db20479df392ae48d9d14287eca92debeda
ghagevaibhav <ghagevaibhav@gmail.com>
2024-10-30 16:30:39 +0530

    default route

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,23 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return Response.json({
-			message: 'Hello, world!',
-		});
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
-} satisfies ExportedHandler<Env>;
+};
